15d7628ec368cd0c4fca2d09b3f93c99ae43fdca
NILUFAR <n1lufarmirathamova@gmail.com>
2023-03-15 07:03:20 +0500

    js change

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const audio = new Audio("ES_Button Click - SFX Producer.mp3");
 const buttons = document.querySelectorAll("button");
+const short = document.querySelector(".short")
 
 buttons.forEach(button => {
   button.addEventListener("click", () => {
